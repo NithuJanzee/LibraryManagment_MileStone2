dd3abd86df4bcfb65b16a458665a8e6ff2745434
Nithushan Janson <jansonnithusanofficial@gmail.com>
2024-10-07 21:28:48 +0530

    Deleted unwanted files

--- LibraryFrontend/Index/api.js
+++
@@ -1,8 +0,0 @@
-export async function addUser(obj){
-    await fetch('http://localhost:3000/user',{
-      method:'POST',
-      headers: {'Content-Type': 'application/json'},
-      body: JSON.stringify({"firstname":obj.fname, "lastname":obj.lname, "nic":obj.nic, "email":obj.mail, "phone":obj.tpnumber, "password":obj.pw})
-    })
-    
-  }
