fd888fdc6ec37b11ae691ed8492162bb82bee009
Nithushan J. <UT01268@unicomtic.com>
2024-10-07 12:44:09 +0530

    bookCoverSlidShowWork!

--- LibraryManagmentAPI/LibraryManagment/obj/Debug/net8.0/LibraryManagment.AssemblyInfo.cs
+++ LibraryManagmentAPI/LibraryManagment/obj/Debug/net8.0/LibraryManagment.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LibraryManagment")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ba22101c73376d5d3d79829e665345c8b81aeb62")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b9dd0319916b3f3573e853ba03930381db16770f")]
 [assembly: System.Reflection.AssemblyProductAttribute("LibraryManagment")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LibraryManagment")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
