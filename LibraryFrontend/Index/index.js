7f82e827bae70b7ed37caf168c39023a091259bf
Nithushan J. <UT01268@unicomtic.com>
2024-10-14 11:48:29 +0530

    updatedIndexPageSignInAndSignUp

--- LibraryFrontend/Index/index.js
+++ LibraryFrontend/Index/index.js
@@ -1,5 +1,5 @@
-
 const messages = document.getElementById("ThrowMessages");
+const messages1 = document.getElementById('ThrowMessages1');
 //SignUp
 const signUpButton = document.getElementById("signUpBtn");
 signUpButton.addEventListener("click", async (event) => {
@@ -29,7 +29,7 @@ signUpButton.addEventListener("click", async (event) => {
   const PostUser = async (User) => {
     const response = await fetch(`http://localhost:5000/api/User/AddUsers`, {
       method: "POST",
-      headers: { "Content-Type": "application/json" }, 
+      headers: { "Content-Type": "application/json" },
       body: JSON.stringify(User),
     });
     return response;
@@ -37,9 +37,64 @@ signUpButton.addEventListener("click", async (event) => {
   const theResponse = await PostUser(User);
 
   if (theResponse.ok) {
+    setTimeout(() => {
+      closeSignUp(()=>{
+        openSignIn();
+      })
+    }, 1000)
     messages.innerHTML = '<p class="Gmessage">User created successfully! Please signIn</p>';
+    
 
   } else {
     messages.innerHTML = '<p id="Bmessage">Nic Already exits!</p>';
   }
+
+  function openSignIn() {
+    var signInModal = new bootstrap.Modal(document.getElementById('signInModal'));
+    signInModal.show();
+  }
+
+  function closeSignUp(callback) {
+    var signUpModal = bootstrap.Modal.getInstance(document.getElementById('signUpModal'));
+    if (signUpModal) {
+        signUpModal.hide(); 
+        if (callback && typeof callback === 'function') {
+            callback(); 
+        }
+    }
+}
+
 });
+
+
+const SignInButton = document.getElementById('SignInBtn');
+SignInButton.addEventListener('click', async (event) => {
+  event.preventDefault();
+  const nic = document.getElementById('nicSignIn').value;
+  const password = document.getElementById('passwordSignIn').value;
+
+  const login = {
+    nic: nic,
+    password: password
+  }
+
+  const CheckUser = async () => {
+    const postData = await fetch(`http://localhost:5000/api/User/UserLogin`, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(login),
+
+    })
+    return postData;
+  }
+
+  const response = await CheckUser();
+  const bool = await response.json();
+
+  if (bool == true) {
+    messages1.innerHTML = '<p class="Gmessage">LogIn successful</p>';
+  }
+  else {
+    messages1.innerHTML = '<p id="Bmessage">Please Check your Nic and Password</p>';
+  }
+})
